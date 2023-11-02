169a7df6ae430d7eb34496e84e3b517b5319a7b6
LunaSeki <LunaSeki@outlook.com>
2023-11-03 01:17:16 +0800

    feat(navbar): 更新导航栏分类列表

--- src/components/TheNavbar/index.tsx
+++ src/components/TheNavbar/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import SVGIcon, { SVGIconName } from "../SVGIcon";
+import SVGIcon from "../SVGIcon";
 import style from "./Navbar.module.scss";
+import memuList from "./navbarList";
 
 function Navbar() {
-	const [activeNav, setActiveNav] = React.useState<string>("home");
+	const [activeNav, setActiveNav] = React.useState<string | number>("home");
 	return (
 		<div className={style.navbar}>
 			{/* <title>Fleeting Flow</title> */}
@@ -25,7 +26,9 @@ function Navbar() {
 									<li
 										key={item.key}
 										data-active={isActive}
-										onClick={() => setActiveNav(item.key)}
+										onClick={() => {
+											setActiveNav(item.key);
+										}}
 									>
 										<SVGIcon
 											name={item.icon}
@@ -43,98 +46,5 @@ function Navbar() {
 		</div>
 	);
 }
-
-type MemuItem = {
-	name: string;
-	key: string;
-	icon: SVGIconName;
-};
-
-const memuList: MemuItem[][] = [
-	[
-		{
-			name: "首页",
-			key: "home",
-			icon: "home",
-		},
-		{
-			name: "推荐",
-			key: "favourite",
-			icon: "favourite",
-		},
-		{
-			name: "关注",
-			key: "person_add",
-			icon: "person_add",
-		},
-		{
-			name: "朋友",
-			key: "group",
-			icon: "group",
-		},
-		{
-			name: "我的",
-			key: "person",
-			icon: "person",
-		},
-	],
-	[
-		{
-			name: "直播",
-			key: "live_tv",
-			icon: "live_tv",
-		},
-		{
-			name: "放映厅",
-			key: "theaters",
-			icon: "theaters",
-		},
-		{
-			name: "知识",
-			key: "book",
-			icon: "book",
-		},
-		{
-			name: "热点",
-			key: "fire",
-			icon: "fire",
-		},
-		{
-			name: "游戏",
-			key: "sports_esports",
-			icon: "sports_esports",
-		},
-		{
-			name: "娱乐",
-			key: "kid_star",
-			icon: "kid_star",
-		},
-		{
-			name: "二次元",
-			key: "cruelty_free",
-			icon: "cruelty_free",
-		},
-		{
-			name: "音乐",
-			key: "library_music",
-			icon: "library_music",
-		},
-		{
-			name: "美食",
-			key: "dinner_dining",
-			icon: "dinner_dining",
-		},
-		{
-			name: "体育",
-			key: "sports_basketball",
-			icon: "sports_basketball",
-		},
-		{
-			name: "时尚",
-			key: "fashion",
-			icon: "diamond",
-		},
-	],
-];
 
 export default Navbar;
---
+++ src/components/TheNavbar/navbarList.ts
@@ -0,0 +1,101 @@
+import { SVGIconName } from "../SVGIcon";
+
+export type MemuItem = {
+	name: string;
+	key: number | string;
+	icon: SVGIconName;
+};
+
+const memuList: MemuItem[][] = [
+	[
+		{
+			name: "首页",
+			key: "home",
+			icon: "home",
+		},
+		{
+			name: "推荐",
+			key: "favourite",
+			icon: "favourite",
+		},
+		{
+			name: "关注",
+			key: "person_add",
+			icon: "person_add",
+		},
+		{
+			name: "朋友",
+			key: "group",
+			icon: "group",
+		},
+		{
+			name: "我的",
+			key: "person",
+			icon: "person",
+		},
+	],
+	[
+		// {
+		// 	name: "直播",
+		// 	key: "live_tv",
+		// 	icon: "live_tv",
+		// },
+		{
+			name: "宠物",
+			key: 1,
+			icon: "pet_supplies",
+		},
+		{
+			name: "知识",
+			key: 2,
+			icon: "book",
+		},
+		{
+			name: "解说",
+			key: 3,
+			icon: "theaters",
+		},
+		// {
+		// 	name: "热点",
+		// 	key: "fire",
+		// 	icon: "fire",
+		// },
+		{
+			name: "游戏",
+			key: 4,
+			icon: "sports_esports",
+		},
+		{
+			name: "娱乐",
+			key: 5,
+			icon: "kid_star",
+		},
+		{
+			name: "二次元",
+			key: 6,
+			icon: "cruelty_free",
+		},
+		{
+			name: "音乐",
+			key: 7,
+			icon: "library_music",
+		},
+		{
+			name: "美食",
+			key: 8,
+			icon: "dinner_dining",
+		},
+		{
+			name: "体育",
+			key: 9,
+			icon: "sports_basketball",
+		},
+		{
+			name: "时尚",
+			key: 10,
+			icon: "diamond",
+		},
+	],
+];
+
+export default memuList;
